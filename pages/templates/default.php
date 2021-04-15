c9f6c3f45484effd212dc58bf796fe4aa4bc2e03
franckalainai <franckalain.ai@gmail.com>
2021-04-15 10:53:01 +0000

    reorganisation de la structure

---
+++ pages/templates/default.php
@@ -0,0 +1,58 @@
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.82.0">
+    <title>Top navbar example · Bootstrap v5.0</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+ 
+
+    <!-- Bootstrap core CSS -->
+
+
+
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>
+
+    
+    <!-- Custom styles for this template -->
+    <link href="navbar-top.css" rel="stylesheet">
+  </head>
+  <body>
+    
+<nav class="navbar navbar-expand-md navbar-dark bg-dark mb-4">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Project Name</a>
+  </div>
+</nav>
+
+<main class="container">
+  <div class="bg-light p-5 rounded">
+      <?= $content; ?>
+  </div>
+</main>
+
+
+    <script src="/docs/5.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+
+      
+  </body>
+</html>
--- public/index.php
+++ public/index.php
@@ -9,8 +9,12 @@ if(isset($_GET['p'])){
     $p = 'home';
 }
 
+ob_start();
 if($p === 'home'){
     require '../pages/home.php';
 }elseif($p === 'single'){
     require '../pages/single.php';
 }   
+$content = ob_get_clean();
+
+require '../pages/templates/default.php';
